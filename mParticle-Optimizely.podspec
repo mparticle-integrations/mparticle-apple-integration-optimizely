e7c9020ab790b81304e9a97f2c1b66dde8b16105
BrandonStalnaker <bstalnaker@mparticle.com>
2020-02-11 16:57:48 -0500

    Update to Optimizely Swift SDK

--- mParticle-Optimizely.podspec
+++ mParticle-Optimizely.podspec
@@ -12,21 +12,16 @@ Pod::Spec.new do |s|
     s.author           = { "mParticle" => "support@mparticle.com" }
     s.source           = { :git => "https://github.com/mparticle-integrations/mparticle-apple-integration-optimizely.git", :tag => s.version.to_s }
     s.social_media_url = "https://twitter.com/mparticle"
+    s.dependency 'mParticle-Apple-SDK/mParticle', '~> 7.0'
+    s.dependency 'OptimizelySwiftSDK', '~> 3.0'
+    s.swift_versions = ['5.0']
 
-    s.ios.deployment_target = "8.0"
+    s.ios.deployment_target = "10.0"
     s.ios.source_files      = 'mParticle_Optimizely/*.{h,m,mm}'
-    s.ios.dependency 'mParticle-Apple-SDK/mParticle', '~> 7.0'
     s.ios.frameworks = 'CoreTelephony', 'SystemConfiguration'
     s.libraries = 'z'
-    s.ios.dependency 'OptimizelySDKiOS', '~> 3.0'
 
-    s.tvos.deployment_target = "9.0"
+    s.tvos.deployment_target = "10.0"
     s.tvos.source_files      = 'mParticle_Optimizely/*.{h,m,mm}'
-    s.tvos.dependency 'mParticle-Apple-SDK/mParticle', '~> 7.0'
     s.tvos.frameworks = 'SystemConfiguration'
-    s.tvos.dependency 'OptimizelySDKTVOS', '~> 3.0'
-    
-    s.tvos.pod_target_xcconfig = {
-        'LIBRARY_SEARCH_PATHS' => '$(inherited) $(PODS_ROOT)/OptimizelySDKTVOS/**'
-    }
 end
--- mParticle_Optimizely/MPKitOptimizely.h
+++ mParticle_Optimizely/MPKitOptimizely.h
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+
 #if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle.h>)
 #import <mParticle_Apple_SDK/mParticle.h>
 #else
@@ -9,8 +10,7 @@ extern NSString * _Nonnull const optimizelyCustomEventName;
 extern NSString * _Nonnull const optimizelyTrackedValue;
 extern NSString * _Nonnull const optimizelyCustomUserId;
 
-@class OPTLYClient;
-@class OPTLYVariation;
+@class OptimizelyClient;
 
 extern NSString * _Nonnull const MPKitOptimizelyEventName;
 extern NSString * _Nonnull const MPKitOptimizelyEventKeyValue;
@@ -22,7 +22,7 @@ extern NSString * _Nonnull const MPKitOptimizelyEventKeyValue;
 @property (nonatomic, unsafe_unretained, readonly) BOOL started;
 @property (nonatomic, strong, nullable) MPKitAPI *kitApi;
 
-- (OPTLYVariation *_Nullable)variationForExperimentKey:(nonnull NSString *)key customUserId:(nullable NSString *)customUserID;
-+ (OPTLYClient *_Nullable)optimizelyClient;
-+ (void)setOptimizelyClient:(OPTLYClient *_Nullable)client;
+- (NSString *_Nullable)activateWithExperimentKey:(nonnull NSString *)key customUserId:(nullable NSString *)customUserID;
++ (OptimizelyClient *_Nullable)optimizelyClient;
++ (void)setOptimizelyClient:(OptimizelyClient *_Nullable)client;
 @end
