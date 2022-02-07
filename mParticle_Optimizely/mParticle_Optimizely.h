062bbea935b9812e0c37f1ff6635095fed1063b4
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2022-02-07 12:03:46 -0500

    fix: fix spm imports (#14)

--- mParticle_Optimizely/mParticle_Optimizely.h
+++ mParticle_Optimizely/mParticle_Optimizely.h
@@ -4,4 +4,8 @@ FOUNDATION_EXPORT double mParticle_OptimizelyVersionNumber;
 
 FOUNDATION_EXPORT const unsigned char mParticle_OptimizelyVersionString[];
 
+#if defined(__has_include) && __has_include(<mParticle_Optimizely/MPKitOptimizely.h>)
 #import <mParticle_Optimizely/MPKitOptimizely.h>
+#else
+#import "MPKitOptimizely.h"
+#endif
